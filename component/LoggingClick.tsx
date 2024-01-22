40590c890f696783f5dca8663d77630618c35d73
sangminnn <psm0102psm@gmail.com>
2024-01-22 21:17:57 +0900

    imp: Logging 컴포넌트의 props drilling을 해소하기위한 context api 활용방안 메모완료

--- component/LoggingClick.tsx
+++ component/LoggingClick.tsx
@@ -1,5 +1,30 @@
 import React from "react";
 
+import React, { createContext, useContext } from "react";
+
+interface LogParamsContext {
+  params: Log;
+  children: React.ReactNode;
+}
+
+const LogParamsContext = createContext<Log | null>(null);
+
+export function useLogParams() {
+  const context = useContext(LogParamsContext);
+  if (context === undefined) {
+    throw new Error("useLogParams는 LogParamsProvider 하위에 있어야합니다.");
+  }
+  return context;
+}
+
+const LogParamsProvider = ({ params, children }: LogParamsContext) => {
+  return (
+    <LogParamsContext.Provider value={params}>
+      {children}
+    </LogParamsContext.Provider>
+  );
+};
+
 type LoggingType = "select" | "custom";
 
 interface Props {
