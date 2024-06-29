886515f6efc3a1a2417cb48b2cc72bd94d7db3fe
sangminnn <psm0102psm@gmail.com>
2024-06-30 00:43:41 +0900

    imp: AspectRatio 컴포넌트 개선 적용 완료

--- component/AspectRatio.tsx
+++ component/AspectRatio.tsx
@@ -1,61 +1,63 @@
 import React, { PropsWithChildren } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-interface Props {
-  as: Element;
+type BaseProps = {
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+interface PropsWithWidthAndHeight extends BaseProps {
   width: number;
   height: number;
-  style: React.CSSProperties;
+  ratio?: never;
+}
+
+interface PropsWithRatio extends BaseProps {
+  width?: never;
+  height?: never;
   ratio: number;
 }
 
-const aspectRatio = ({
-  ratio,
-  tag,
-}: {
-  ratio: number;
-  tag: keyof JSX.IntrinsicElements;
-}) => css`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  padding-top: calc(100% * ${ratio});
-
-  & > ${tag} {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`;
-
+type Props = PropsWithWidthAndHeight | PropsWithRatio;
 const AspectRatio = ({
-  as,
+  className,
   style,
   width,
   height,
+  ratio,
   children,
 }: PropsWithChildren<Props>) => {
-  const ratio = height / width;
+  const calculatedRatio = ratio || height! / width!;
+
   return (
-    <RatioWrapper style={style} ratio={ratio} as={as}>
+    <Wrapper className={className} style={style} ratio={calculatedRatio}>
       {children}
-    </RatioWrapper>
+    </Wrapper>
   );
 };
 
-const RatioWrapper = styled.div<{ ratio: number }>`
-  position: relative;
-  width: 100%;
-  padding-top: ${({ ratio }) => ratio && `calc(100% * ${ratio})`};
+const Wrapper = styled.div<{ style?: React.CSSProperties; ratio: number }>`
+  & > img,
+  & > video {
+    object-fit: cover;
+  }
 
-  & > * {
-    position: absolute;
+  @supports (aspect-ratio: 1 / 1) {
+    aspect-ratio: ${({ ratio }) => ratio && `${ratio}`};
+  }
+
+  @supports not (aspect-ratio: 1 / 1) {
+    position: relative;
     width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
+    padding-top: ${({ ratio }) => ratio && `calc(100% * ${ratio})`};
+
+    & > *:not(style) {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      left: 0;
+      top: 0;
+    }
   }
 `;
 
