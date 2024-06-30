420a2b361e9aa151276b33874529a53ac3aa5643
sangminnn <psm0102psm@gmail.com>
2024-06-30 12:33:53 +0900

    imp: AspectRatio 컴포넌트 추가 개선 적용 완료

--- component/AspectRatio.tsx
+++ component/AspectRatio.tsx
@@ -1,37 +1,23 @@
-import React, { PropsWithChildren } from "react";
+import React, { Children, PropsWithChildren } from "react";
 import styled from "styled-components";
 
-type BaseProps = {
+interface Props {
   className?: string;
   style?: React.CSSProperties;
-};
-
-interface PropsWithWidthAndHeight extends BaseProps {
-  width: number;
-  height: number;
-  ratio?: never;
-}
-
-interface PropsWithRatio extends BaseProps {
-  width?: never;
-  height?: never;
   ratio: number;
 }
 
-type Props = PropsWithWidthAndHeight | PropsWithRatio;
 const AspectRatio = ({
   className,
   style,
-  width,
-  height,
   ratio,
   children,
 }: PropsWithChildren<Props>) => {
-  const calculatedRatio = ratio || height! / width!;
+  const child = Children.only(children);
 
   return (
-    <Wrapper className={className} style={style} ratio={calculatedRatio}>
-      {children}
+    <Wrapper className={className} style={style} ratio={ratio}>
+      {child}
     </Wrapper>
   );
 };
@@ -42,22 +28,16 @@ const Wrapper = styled.div<{ style?: React.CSSProperties; ratio: number }>`
     object-fit: cover;
   }
 
-  @supports (aspect-ratio: 1 / 1) {
-    aspect-ratio: ${({ ratio }) => ratio && `${ratio}`};
-  }
+  position: relative;
+  width: 100%;
+  padding-top: ${({ ratio }) => ratio && `calc(100% * ${1 / ratio})`};
 
-  @supports not (aspect-ratio: 1 / 1) {
-    position: relative;
+  & > *:not(style) {
+    position: absolute;
     width: 100%;
-    padding-top: ${({ ratio }) => ratio && `calc(100% * ${ratio})`};
-
-    & > *:not(style) {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      left: 0;
-      top: 0;
-    }
+    height: 100%;
+    left: 0;
+    top: 0;
   }
 `;
 
