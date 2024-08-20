57d8a5f2570c472c33586373c642f7440598da70
sangminnn <psm0102psm@gmail.com>
2024-08-21 00:08:22 +0900

    imp: useElementDimensions 훅의 타겟 element도 추론될 수 있도록 타입 수정 완료

--- hook/useElementDimensions.ts
+++ hook/useElementDimensions.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
 type Nullable<T> = T | null;
 
@@ -13,28 +13,26 @@ type ElementDimensions = {
   offsetTop: number;
 };
 
-type Values<T> = T[keyof T];
 type ElementDimensionsKeys = keyof ElementDimensions;
-type Dimensions<T extends ElementDimensionsKeys[]> = {
-  [K in T[number]]: ElementDimensions[K];
-};
 
-function useElementDimensions<T extends ElementDimensionsKeys[]>(...keys: T) {
-  const ref = useRef<HTMLElement>(null);
-  const [dimensions, setDimensions] = useState<Nullable<Dimensions<T>>>(null);
+function useElementDimensions<
+  T extends HTMLElement,
+  K extends ElementDimensionsKeys = ElementDimensionsKeys,
+>(...keys: K[]) {
+  const ref = useRef<T>(null);
+  const [dimensions, setDimensions] =
+    useState<Nullable<Pick<ElementDimensions, K>>>(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const element = ref.current;
     if (element == null) {
       return;
     }
 
-    const dimensionData = {} as { [K in T[number]]: ElementDimensions[K] };
+    const dimensionData = {} as Pick<ElementDimensions, K>;
 
     for (const key of keys) {
-      dimensionData[key as T[number]] = element[
-        key
-      ] as Values<ElementDimensions>;
+      dimensionData[key] = element[key];
     }
 
     setDimensions(dimensionData);
