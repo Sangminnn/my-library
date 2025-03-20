acbd80ed8819ef6ae5eaf6b33e176de5e69ea91b
sangminnn <psm0102psm@gmail.com>
2025-03-20 22:43:47 +0900

    feat: useIsClient 훅 추가

---
+++ hook/useIsClient.ts
@@ -0,0 +1,9 @@
+import { useSyncExternalStore } from "react";
+
+import { noop } from "@/utils/noop";
+
+const emptySubscribe = () => noop;
+const getSnapshot = () => true;
+const getServerSnapshot = () => false;
+export const useIsClient = () =>
+  useSyncExternalStore(emptySubscribe, getSnapshot, getServerSnapshot);
